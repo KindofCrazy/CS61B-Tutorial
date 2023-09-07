6adcb0b294185e40f1f6f5e4c05951da8aadfeee
Yang Ke <1652467250@qq.com>
2023-09-07 16:50:06 +0800

    Update

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -10,20 +13,53 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private int t;
+    private Maze maze;
+
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        // Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new ConcurrentLinkedQueue<>();
+        queue.add(s);
+        marked[s] = true;
+        announce();
+
+        if (s == t) {
+            return;
+        }
+
+        while (!queue.isEmpty()) {
+            int removedValue = queue.remove();
+            for (int w : maze.adj(removedValue)) {
+                if (!marked[w]) {
+                    edgeTo[w] = removedValue;
+                    distTo[w] = distTo[removedValue] + 1;
+                    marked[w] = true;
+                    announce();
+                    if (w == t) {
+                        return;
+                    }
+                    queue.add(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
