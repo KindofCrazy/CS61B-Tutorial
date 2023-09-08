34cdd3bf56ab0fc12fe7f088e7c619980a81eb1e
Yang Ke <1652467250@qq.com>
2023-09-08 14:47:38 +0800

    Complete A* algorithm

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -20,18 +25,70 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
     /** Finds vertex estimated to be closest to target. */
     private int findMinimumUnmarked() {
-        return -1;
+        int minimumIndex = 0;
+        for (int i = 0; i < distTo.length; i++) {
+            if (!marked[i] && distTo[i] != Integer.MAX_VALUE) {
+                minimumIndex = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < distTo.length; i++) {
+            if (distTo[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+
+            if (!marked[i]) {
+                int currentMinPriority = distTo[minimumIndex] + distTo[minimumIndex];
+                int potentialMinPriority = distTo[i] + distTo[i];
+                if (currentMinPriority > potentialMinPriority) {
+                    minimumIndex = i;
+                }
+            }
+        }
+
+        return minimumIndex;
         /* You do not have to use this method. */
     }
 
+    void changePriority(int v) {
+        for (int w : maze.adj(v)) {
+            if (distTo[w] > distTo[v] + 1) {
+                distTo[w] = distTo[v] + 1;
+                edgeTo[w] = v;
+            }
+        }
+    }
+
+
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        Queue<Integer> queue = new ConcurrentLinkedQueue<>();
+        queue.add(s);
+        marked[s] = true;
+        announce();
+
+        if (s == t) {
+            return;
+        }
+
+        while (!queue.isEmpty()) {
+            int removedValue = queue.remove();
+            changePriority(removedValue);
+            int nextNode = findMinimumUnmarked();
+            marked[nextNode] = true;
+            announce();
+            if (nextNode == t) {
+                return;
+            }
+            queue.add(nextNode);
+        }
+
     }
 
     @Override
